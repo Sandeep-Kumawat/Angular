81ebd7a955dc41d48486525b2d39b08273af103a
Sandeep Kumawat <sandeep.kumawat@taazaa.com>
2021-04-01 18:14:51 +0530

    Binding And Component Communication

--- Binding/src/app/app.component.ts
+++ Binding/src/app/app.component.ts
@@ -9,13 +9,22 @@ export class AppComponent {
   title = 'Angular Demo';
   showDataBindingDemo = true;
   showDirectiveDemo = false;
+  showPurchaseDemo = false;
   showdbdemo():void{
     this.showDataBindingDemo = true;
     this.showDirectiveDemo = false;
+    this.showPurchaseDemo = false;
   }
   showdirecyivedemo():void{
     this.showDataBindingDemo = false;
     this.showDirectiveDemo = true;
+    this.showPurchaseDemo = false;
+  }
+  showPurchase():void{
+    this.showPurchaseDemo = true;
+    this.showDataBindingDemo = false;
+    this.showDirectiveDemo = false;
+    
   }
 
 }
